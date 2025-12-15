8709a387629048044f34d10ebded2003aad8a773
hs8848 <harishsundaram20@gmail.com>
2025-12-15 18:09:16 +0530

    Added search feature

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,14 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
-
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import permissions
 from profiles_api import serializers
 from profiles_api import models
-
-from rest_framework.authentication import TokenAuthentication
-from profiles_api import permissions
 
 class HelloAPIView(APIView):
     """Test API View"""
@@ -123,3 +122,6 @@ class UserProileViewset(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authenticaion_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+    
